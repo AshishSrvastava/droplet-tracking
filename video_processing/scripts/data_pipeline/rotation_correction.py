21d12e09f7ae94db28c0274f462731dd60758afc
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-04-02 18:30:40 -0600

    progress bar and rotation working properly in rotation_correction.py. Correctly outputting videos to rotated_videos/

--- video_processing/scripts/data_pipeline/rotation_correction.py
+++ video_processing/scripts/data_pipeline/rotation_correction.py
@@ -15,23 +15,6 @@ def create_folders():
         os.makedirs("position_data")
 
 
-# def select_points(event, x, y, flags, param):
-#     """Mouse event callback function"""
-#     points, selecting, first_frame_display = param  # retrieve points from param
-
-#     if event == cv2.EVENT_MOUSEMOVE:
-#         update_first_frame_display(x, y, points, first_frame_display, selecting)
-
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         if len(points) < 2:
-#             points.append((x, y))
-#             print(f"Point {len(points)}: ({x}, {y})")
-#             update_first_frame_display(x, y, points, first_frame_display, selecting)
-#             if len(points) == 2:
-#                 selecting = False
-#                 # print(f"Selecting is done. Points: {points}")
-#                 return points  # return points when selecting is done
-
 def select_points(event, x, y, flags, param):
     """Mouse event callback function"""
     points, selecting, first_frame_display = param  # retrieve points from param
@@ -45,9 +28,14 @@ def select_points(event, x, y, flags, param):
             print(f"Point {len(points)}: ({x}, {y})")
             if len(points) == 2:
                 selecting = False
-                print(f"Selecting is done. Points: {points}")
+                print(f"Selecting is done. Confirmed points: {points}")
             update_first_frame_display(x, y, points, first_frame_display, selecting)
-            
+
+    cv2.imshow("First Frame", first_frame_display)
+    cv2.waitKey(1)
+    return points
+
+
 
 
 def update_first_frame_display(x, y, points, first_frame_display, selecting):
@@ -69,56 +57,59 @@ def update_first_frame_display(x, y, points, first_frame_display, selecting):
         cv2.circle(first_frame_display_copy, points[1], 5, (0, 0, 255), -1)
         cv2.putText(first_frame_display_copy, "Selected", (points[1][0] + 5, points[1][1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
-    cv2.imshow("First Frame", first_frame_display_copy)
-    
-    # for i, point in enumerate(points):
-    #     cv2.circle(first_frame_display_copy, point, 5, (0, 0, 255), -1)
-    #     cv2.putText(first_frame_display_copy, f"Point {i + 1}", (point[0] + 5, point[1] + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-
-    # if selecting:
-    #     cv2.circle(first_frame_display_copy, (x, y), 5, (0, 255, 0), -1)
-
-    # cv2.imshow("First Frame", first_frame_display_copy)
-
         
-def get_correction_angle(input_vid, first_frame_display):
-    """Get the correction angle from the input video filename"""
-    # global points
-    points = []
-    selecting = True
+# def get_correction_angle(input_vid, first_frame_display):
+#     """Get the correction angle from the input video filename"""
+#     # global points
+#     points = []
+#     selecting = True
     
-    # Check if angle is already in the input video filename
-    angle_pattern = re.compile(r"[-+]?\d+_\d+(?=_angle)")  # Updated regular expression
-    angle_match = angle_pattern.search(input_vid)
+#     # Check if angle is already in the input video filename
+#     angle_pattern = re.compile(r"[-+]?\d+_\d+(?=_angle)")  # Updated regular expression
+#     angle_match = angle_pattern.search(input_vid)
 
-    if angle_match:
-        angle_str = angle_match.group().replace("_", ".")
-        correction_angle = float(angle_str)
-        print(f"Correction angle from filename: {correction_angle} degrees")
-    else:
-        # Show first frame for user to select points
-        cv2.namedWindow("First Frame")
-        # cv2.setMouseCallback("First Frame", select_points)
-        cv2.setMouseCallback("First Frame", select_points, param=(points, selecting, first_frame_display)) # pass points to select_points
+#     if angle_match:
+#         angle_str = angle_match.group().replace("_", ".")
+#         correction_angle = float(angle_str)
+#         print(f"Correction angle from filename: {correction_angle} degrees")
+#     else:
+#         # Show first frame for user to select points
+#         cv2.namedWindow("First Frame")
+#         # cv2.setMouseCallback("First Frame", select_points)
+#         cv2.setMouseCallback("First Frame", select_points, param=(points, selecting, first_frame_display)) # pass points to select_points
 
-        while selecting:
-            cv2.imshow("First Frame", first_frame_display)
-            key = cv2.waitKey(1) & 0xFF
-            if key == 27 or key == ord("q"):
-                exit(0)
+#         while selecting:
+#             cv2.imshow("First Frame", first_frame_display)
+#             key = cv2.waitKey(1) & 0xFF
+#             if key == 27 or key == ord("q"):
+#                 exit(0)
 
-        cv2.destroyAllWindows()
+#         cv2.destroyAllWindows()
 
-            # Fit a line to the two points and calculate the angle
-        x1, y1 = points[0]
-        x2, y2 = points[1]
-        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        angle = round(angle, 2)
+#             # Fit a line to the two points and calculate the angle
+#         x1, y1 = points[0]
+#         x2, y2 = points[1]
+#         angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+#         angle = round(angle, 2)
 
-        correction_angle = angle
-        print(f"Correction angle: {correction_angle} degrees")
+#         correction_angle = angle
+#         print(f"Correction angle: {correction_angle} degrees")
     
-    return correction_angle
+#     return correction_angle
+
+def get_correction_angle(points):
+    """Get the correction angle from the selected points"""
+    print(f"Points passed into get_correction_angle: {points}")
+    print(f"get_correction_angle called. Points: {points}")
+    x1, y1 = points[0]
+    x2, y2 = points[1]
+    angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    angle = round(angle, 2)
+
+    print(f"Correction angle: {angle} degrees")
+
+    return angle
+
         
 # Helper function to rotate a frame
 def rotate_frame(frame, angle):
@@ -128,29 +119,106 @@ def rotate_frame(frame, angle):
     rotated = cv2.warpAffine(frame, M, (w, h))
     return rotated
         
+# def rotate_video(input_video_path):
+#     """Rotate a video and save the rotated video to the 'rotated_videos' folder"""
+#     global points, selecting
+    
+#     # Load the video
+#     cap = cv2.VideoCapture(input_video_path)
+
+#     if not cap.isOpened():
+#         print(f"Error: Could not open {input_video_path}")
+#         return
+    
+#     # Get the first frame
+#     ret, first_frame = cap.read()
+    
+#     # Make a copy of the first frame to display
+#     first_frame_display = first_frame.copy()
+    
+#     # Initialize selecting and points variables
+#     selecting = True
+#     points = []
+
+#     # Get the correction angle
+#     correction_angle = get_correction_angle(input_video_path, first_frame_display)
+
+#     # Check the length of the points variable
+#     while selecting:
+#         cv2.imshow("First Frame", first_frame_display)
+#         key = cv2.waitKey(1) & 0xFF
+#         if key == 27 or key == ord("q"):
+#             exit(0)
+#         if len(points) == 2:
+#             selecting = False
+
+#     cv2.destroyAllWindows()
+
+#     # Output video
+#     fourcc = cv2.VideoWriter_fourcc(*"DIVX")
+#     base_name = os.path.splitext(os.path.basename(input_video_path))[0]
+#     angle_str = f"{correction_angle:.2f}".replace(".", "_") + "deg"
+#     output_video_path = f"rotated_videos/{base_name}_{angle_str}_rotated.avi"
+#     print(f"Output video: {output_video_path}")
+#     out = cv2.VideoWriter(output_video_path, fourcc, 30, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+#     num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+#     for frame_num in range(num_frames):
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         # Rotate the frame using the angle
+#         print(f"Correction angle: {correction_angle} degrees")
+#         rotated_frame = rotate_frame(frame, correction_angle)
+
+#         # Write the rotated frame to the output video
+#         out.write(rotated_frame)
+
+#         # Calculate progress and display a progress bar        progress = int((frame_num / num_frames) * 100)
+#         bar_width = 50
+#         filled_width = int(bar_width * progress / 100)
+#         remaining_width = bar_width - filled_width
+#         print(f"[{'=' * filled_width}{' ' * remaining_width}] {progress}%\r", end="")
+
+
+#     cap.release()
+#     out.release()
+#     cv2.destroyAllWindows()
+
 def rotate_video(input_video_path):
     """Rotate a video and save the rotated video to the 'rotated_videos' folder"""
-    global points, selecting
-    
+    points = []
+    selecting = True
+
     # Load the video
     cap = cv2.VideoCapture(input_video_path)
 
     if not cap.isOpened():
         print(f"Error: Could not open {input_video_path}")
         return
-    
+
     # Get the first frame
     ret, first_frame = cap.read()
-    
+
     # Make a copy of the first frame to display
     first_frame_display = first_frame.copy()
-    
-    # Initialize selecting and points variables
-    selecting = True
-    points = []
 
+    # # Get the correction angle
+    # correction_angle = get_correction_angle(points)
+
+    # Show first frame for user to select points
+    cv2.namedWindow("First Frame")
+    cv2.setMouseCallback("First Frame", select_points, param=(points, selecting, first_frame_display)) # pass points to select_points
+
+    while len(points) < 2:
+        cv2.imshow("First Frame", first_frame_display)
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27 or key == ord("q"):
+            exit(0)
+    
     # Get the correction angle
-    correction_angle = get_correction_angle(input_video_path, first_frame_display)
+    correction_angle = get_correction_angle(points)
 
     # Output video
     fourcc = cv2.VideoWriter_fourcc(*"DIVX")
@@ -160,23 +228,32 @@ def rotate_video(input_video_path):
     print(f"Output video: {output_video_path}")
     out = cv2.VideoWriter(output_video_path, fourcc, 30, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
-    # Rotate the video and write to output
-    while cap.isOpened():
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    for frame_num in range(num_frames):
         ret, frame = cap.read()
         if not ret:
             break
 
         # Rotate the frame using the angle
+        # print(f"Correction angle inside rotation loop: {correction_angle} degrees") # debug
         rotated_frame = rotate_frame(frame, correction_angle)
 
         # Write the rotated frame to the output video
         out.write(rotated_frame)
-        cv2.imshow("Rotated Video", rotated_frame)
-        cv2.waitKey(1)
+
+        # Calculate progress and display a progress bar     
+        # Calculate progress and display a progress bar
+        progress = int((frame_num / num_frames) * 100)  # Add this line to define progress
+        bar_width = 50
+        filled_width = int(bar_width * progress / 100)
+        remaining_width = bar_width - filled_width
+        print(f"[{'=' * filled_width}{' ' * remaining_width}] {progress}%\r", end="")
+
 
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
